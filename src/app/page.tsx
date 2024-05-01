76ee3a89db52a5a5518d295aedd1b7b49a706309
Colin Spangelo <colin.spangelo@gmail.com>
2024-05-01 11:34:11 -0600

    Created todos using next and typescript.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
+import { prisma } from "./db";
+import TodoItem from "./components/TodoItem";
 
-export default function Home() {
+function getTodos(){
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo( id: string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({where: {id}, data: {complete}})
+}
+
+
+export default async function Home() {
+  const todos = await getTodos()
+  // await prisma.todo.create({data: { title: "test", complete: false}})
+  
   return (
-    <h1>Hi</h1>
+    <>
+    <header className="flex justify-between items-center mb-4">
+      <h1 className="text-2xl">Todo App</h1>
+      <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+      href="/new">New</Link>
+    </header>
+    <ul className="pl-4">
+      {todos.map(todo=>(
+        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
+      ))}
+    </ul>
+    </>
   );
 }
